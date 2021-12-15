dac4766d44fb320c76059731c4cd5bd87e3dbf8f
kiwi-fruitiwi <zane.tian@gmail.com>
2021-12-14 23:44:05 -0800

    adding dec-to-binary.py

--- dec-to-binary.py
+++ dec-to-binary.py
@@ -1,1 +1,17 @@
-print("Slugs should have reduced movement ssspeeed")
+number_to_convert = 206
+
+# Part 1:
+first_result = number_to_convert / 2
+first_remainder_lsb = first_result % 2
+
+# Part 2:
+
+
+# Part 3:
+
+
+# Part 4:
+
+# print(f"I convert Decimal to Binary using the 'bin()' function, see: \n52345 = {bin(52345)} in binary.\n\n")
+
+# print(f"I convert Binary to Decimal using the 'int() function,  see: \n1100110001111001 = {int(0b1100110001111001)} in decimal.")
