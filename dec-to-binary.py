02060741b43f0dd299afb57612fdcd4a02b5d279
kiwi-fruitiwi <zane.tian@gmail.com>
2021-12-15 01:40:37 -0800

    finish 6.11 dec-to-bin

--- dec-to-binary.py
+++ dec-to-binary.py
@@ -1,17 +1,22 @@
+# printing in python using something similar to string.format. what is the f?
+
 number_to_convert = 206
 
 # Part 1:
-first_result = number_to_convert / 2
-first_remainder_lsb = first_result % 2
+first_result = 103
+first_remainder_lsb = 0
 
 # Part 2:
-
+third_result = 25
+fourth_remainder = 1
+sixth_result = 3
+final_remainder_msb = 1
 
 # Part 3:
-
+final_binary_number = 11001110
 
 # Part 4:
 
-# print(f"I convert Decimal to Binary using the 'bin()' function, see: \n52345 = {bin(52345)} in binary.\n\n")
+print(f"I convert Decimal to Binary using the 'bin()' function, see: 52345 = {bin(52345)} in binary.\n\n")
 
-# print(f"I convert Binary to Decimal using the 'int() function,  see: \n1100110001111001 = {int(0b1100110001111001)} in decimal.")
+print(f"I convert Binary to Decimal using the 'int() function, see: 1100110001111001 = {int(0b1100110001111001)} in decimal.")
